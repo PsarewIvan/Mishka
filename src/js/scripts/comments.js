0ea7a13ce4197956cff463fed78c4473fca5ee00
Ivan Psarev <psarew.ivan@yandex.ru>
2020-01-05 21:46:07 +0300

    Скрывает лишнии комментарии

---
+++ src/js/scripts/comments.js
@@ -0,0 +1,16 @@
+'use strict';
+{
+  let comments = document.querySelectorAll('.reviews__li');
+
+  function toggleHide(item) {
+    item.classList.toggle('display-none');
+  }
+
+  function hideSpareComment(array) {
+    for(let i = 1; i < array.length; i++) {
+      toggleHide(array[i]);
+    }
+  }
+
+  hideSpareComment(comments);
+}
